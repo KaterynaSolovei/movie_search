c4263c8c15d7ed726919c9bb0890cdb0a829b954
Kateryna Solovei <soloveikat07@gmail.com>
2023-04-23 16:55:42 -0400

    basic version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,21 @@
 const getData = (url) => new Promise((resolve, reject) => {
-  const xhr = new XMLHttpRequests();
+  const xhr = new XMLHttpRequest();
 
   xhr.open('GET', url);
 
   xhr.send();
 
   xhr.onload = () => {
-   if (xhr.status !==200){
+   if (xhr.status !== 200) {
+    reject(xhr.status);
     return;
    }
-    console.log(xhr);
+
+   const json = JSON.parse(xhr.response);
+   resolve(json.Search);
   };
 
-  xhr.onerror = (err) => console.log(err);
+  xhr.onerror = (err) => reject(err);
 });
 
 const search = 'Iron Man';
@@ -20,3 +23,16 @@ const search = 'Iron Man';
 getData(`http://www.omdbapi.com/?apikey=18b8609f&s=${search}`)
 .then((res) =>console.log(res))
 .catch((err) => console.log(err));
+
+/*
+const getData = (url) => fetch(url)
+.then((res) => res.json())
+.then((data) => data.Search);
+
+const search = 'Iron Man';
+
+getData(`http://www.omdbapi.com/?apikey=18b8609f&s=${search}`)
+.then((res) =>console.log(res))
+.catch((err) => console.log(err));
+*/
+
