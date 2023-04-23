40bcacd18b57873935c7d2f03e93f5c32ffef728
Kateryna Solovei <soloveikat07@gmail.com>
2023-04-23 17:56:37 -0400

    practice done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,4 @@
-const getData = (url) => new Promise((resolve, reject) => {
-  const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', url);
-
-  xhr.send();
-
-  xhr.onload = () => {
-   if (xhr.status !== 200) {
-    reject(xhr.status);
-    return;
-   }
-
-   const json = JSON.parse(xhr.response);
-   resolve(json.Search);
-  };
-
-  xhr.onerror = (err) => reject(err);
-});
-
-const search = 'Iron Man';
-
-getData(`http://www.omdbapi.com/?apikey=18b8609f&s=${search}`)
-.then((res) =>console.log(res))
-.catch((err) => console.log(err));
-
-/*
 const getData = (url) => fetch(url)
 .then((res) => res.json())
 .then((data) => data.Search);
@@ -32,7 +6,6 @@ const getData = (url) => fetch(url)
 const search = 'Iron Man';
 
 getData(`http://www.omdbapi.com/?apikey=18b8609f&s=${search}`)
-.then((res) =>console.log(res))
+.then((res) => console.log(res))
 .catch((err) => console.log(err));
-*/
 
---
+++ assets/js/requests.js
@@ -0,0 +1,115 @@
+//Delete => DELETE
+async function deleteData(url) {
+  const response = await fetch(url, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) throw Error(response.status);
+  return true;
+}
+
+deleteData('https://jsonplaceholder.typicode.com/posts/12')
+.then((data) => console.log(data))
+.catch((err) => console.log(err));
+
+
+/*
+//Update => PUT
+async function putData(url, data) {
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  if (!response.ok) throw Error(response.status);
+  return response.json();
+
+}
+
+putData('https://jsonplaceholder.typicode.com/posts/12', {
+  title: 'My title',
+  description: 'My description text',
+  body: 'My body Text',
+  userId: 123
+})
+
+.then((data) => console.log(data))
+.catch((err) => console.log(err));
+*/
+/*
+//Update => PATCH ( will take same info only and replace it)
+async function patchData(url, data) {
+  const response = await fetch(url, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  if (!response.ok) throw Error(response.status);
+  return response.json();
+
+}
+
+patchData('https://jsonplaceholder.typicode.com/posts/12', {
+  title: 'My title',
+  description: 'My description text',
+  body: 'My body Text',
+  userId: 123
+})
+
+.then((data) => console.log(data))
+.catch((err) => console.log(err));
+*/
+
+/*
+//create => POST
+async function postData(url, data) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  if (!response.ok) throw Error(response.status);
+  return response.json();
+
+}
+
+postData('https://jsonplaceholder.typicode.com/posts', {
+  title: 'My title',
+  description: 'My description text',
+  body: 'My body Text',
+  userId: 123
+})
+
+.then((data) => console.log(data))
+.catch((err) => console.log(err));
+*/
+
+/*
+//read => GET
+async function getData(url) {
+  const response = await fetch(url, {
+    method: 'Get',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (!response.ok) throw fetch(url);
+  return response.json();
+
+}
+
+getData('https://jsonplaceholder.typicode.com/posts')
+.then((data) => console.log(data))
+.catch((err) => console.log(err));
+
+*/
